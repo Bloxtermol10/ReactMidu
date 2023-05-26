83b358cf21409e36315ecf72a3d8b71b2dea4be7
Bloxtermol10 <alefrito554@gmail.com>
2023-05-26 05:35:52 -0500

    Estructura usuario twiter

---
+++ Hola mundo/src/app.jsx
@@ -0,0 +1,16 @@
+const App = () =>{
+    return(
+    <article>
+        <header>
+            <img src="https://pbs.twimg.com/profile_images/1540810647604183046/OhYhwdAi_400x400.jpg" alt="Avatar" />
+            <div>  <strong>Ibai</strong>
+                <span>IbaiLlanos</span>
+            </div>
+        </header>
+        <aside>
+            <button>Seguir</button>
+        </aside>
+    </article>)
+}
+
+export default App
