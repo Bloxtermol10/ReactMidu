fb5f1da6f06114cdaa7f5caba8386380376f1f20
Bloxtermol10 <alefrito554@gmail.com>
2023-06-15 04:41:33 -0500

    ouse-folower

--- 03-mouse-follwer/src/App.jsx
+++ 03-mouse-follwer/src/App.jsx
@@ -1,20 +1,38 @@
 import { useEffect, useState } from 'react'
+import "./App.css"
 
 function App () {
-  const [enable, setEnable] = useState(false)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState( { x:0, y:0 } )
 
   useEffect(() => {
-    console.log('efecto', { enable })
-  }, [enable])
+    console.log('effect ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias 
+    // -> antes de ejecutar el efecto de nuevo
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
     <main>
-      <div>
-
-      </div>
+      <div className='circle' style = {{ transform: `translate(${position.x}px, ${position.y}px`}} ></div>
       <h3>Proyecto 3</h3>
-      <button onClick={() => { setEnable(!enable) }}>
-        {enable ? 'Desactivar' : 'Activar'}
+      <button onClick={() => setEnabled(!enabled) }>
+        {enabled ? 'Desactivar' : 'Activar'}
       </button>
     </main>
   )
