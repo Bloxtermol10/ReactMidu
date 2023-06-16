36f5b15ea69e26d16f7878fbb2ce1f21ff29a4c9
Bloxtermol10 <alefrito554@gmail.com>
2023-06-16 05:33:28 -0500

    component twFollowCard Exercice

--- 01-twitter-follow-card/src/logic/app/db.js
+++ 01-twitter-follow-card/src/logic/app/db.js
@@ -1,11 +1,11 @@
-export const diego = { userName: "alefrito554", isFollow: true };
+export const diego = { userName: "Dani Flørëz", isFollow: true };
 export const Users = [
     { id: 1, userName: "IbaiLlanos" },
     { id: 2, userName: "cristiinaruiizz" },
     { id: 3, userName: "blabbergirl_" },
-    { id: 4, userName: "elonmusk" },
+    { id: 4, userName: "SirMaza" },
     { id: 5, userName: "Robleis01" },
-    { id: 6, userName: "arigameplays" },
+    { id: 6, userName: "vegetta777" },
     { id: 7, userName: "samyriveratv" },
     { id: 8, userName: "amablitz" },
 ];
